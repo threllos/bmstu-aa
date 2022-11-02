f4e299c664f9632ab5b2b75cf27db1b0737c3790
thrello <chaoticrnx@gmail.com>
2022-11-02 20:11:17 +0300

    upd benchmark

--- lab_03/src/README.md
+++ lab_03/src/README.md
@@ -1,10 +1,50 @@
-# Лабораторная работа №2
+# Лабораторная работа №3
+
+## Задание
 
 Реализовать:
-- Нерекурсивный метод поиска расстояния Левенштейна (*l_iterative.go*)
-- Нерекурсивный метод поиска Дамерау-Левенштейна (*dl_iterative.go*)
-- Рекурсивный метод поиска Дамерау-Левенштейна (*dl_recursive.go*)
-- Рекурсивный с кешированием метод поска Дамерау-Левенштейна (*dl_recursive_cash.go*)
+- Сортировку вставками (*src/array/sort_insert.go*);
+- Сортировку расчёской (*src/array/sort_comb.pdf*);
+- Блинную сортировку (*src/array/sort_pancake.pdf*).
 
-Unit-tests: *pkg_test.go*
-Benchmark: *pkg_bench_test.go*
+Написать отчет в tex (_docs/report.pdf_).
+
+Unit-tests: *src/array/pkg_test.go*
+Benchmark: *src/benchmark/benchmark.go*
+
+## Программа
+> _использовать в папке src_
+
+**Пользовательский ввод:**
+`make`
+_или_
+`make run`
+_или_
+`make build && ./app.exe`
+
+**Ввод из файла:**
+`make build && ./app.exe < test_data`
+
+**Рандомный ввод:**
+`make build && ./app.exe -r` — случайное заполнение
+`make build && ./app.exe -ra` — заполнение по возрастанию
+`make build && ./app.exe -rd` — заполнение по убыванию
+
+**Изменить количество повторений:**
+_по умолчанию 1000_
+`make build && ./app.exe -repeats=5000`
+
+**Запустить тестирование:**
+`make test`
+
+**Очистить папку:**
+`make clean`
+
+## Отчёт
+> _использовать в папке docs/tex_
+
+**Создать отчёт:**
+`./tex-create.sh`
+
+**Очистить папку:**
+`./tex-clear.sh`
--- lab_03/src/main.go
+++ lab_03/src/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"lab_03/array"
-	"sort"
-	"flag"
+	"lab_03/benchmark"
 	"time"
 )
 
@@ -14,67 +14,6 @@ var rFlag = flag.Bool("r", false, "Random filling array")
 var raFlag = flag.Bool("ra", false, "ASC filling array")
 var rdFlag = flag.Bool("rd", false, "DESC filling array")
 var repeatsFlag = flag.Int("repeats", REPEATS, "Repeats sort")
-
-
-func benchmark(arr array.Array) error {
-	var (
-		durations [4]time.Duration
-		arrs [4]array.Array
-		repeats = *repeatsFlag
-	)
-
-	for i := 0; i < repeats; i++ {
-		arrs[0] = arr.Copy()
-		arrs[1] = arr.Copy()
-		arrs[2] = arr.Copy()
-		arrs[3] = arr.Copy()
-
-		start := time.Now()
-		sort.Sort(arrs[0])
-		durations[0] += time.Since(start)
-
-		start = time.Now()
-		arrs[1].SortComb()
-		durations[1] += time.Since(start)
-
-		start = time.Now()
-		arrs[2].SortInsert()
-		durations[2] += time.Since(start)
-
-		start = time.Now()
-		arrs[3].SortPancake()
-		durations[3] += time.Since(start)
-	}
-
-	if !array.Compare(arrs[0], arrs[1], arrs[2], arrs[3]) {
-		fmt.Println(arrs[0])
-		fmt.Println(arrs[1])
-		fmt.Println(arrs[2])
-		fmt.Println(arrs[3])
-		return fmt.Errorf("arrays not equal after sort")
-	}
-
-	fmt.Printf("\n=== Benchmark ===\n\n")
-
-	if (arr.Len() <= 10) {
-		fmt.Printf("Entry  array: %v\n", arr)
-		fmt.Printf("Sorted array: %v\n\n", arrs[0])
-	}
-
-	fmt.Printf("Random : %v\n", *rFlag)
-	fmt.Printf("Repeats: %d\n", repeats)
-	fmt.Printf("Length : %d\n\n", arr.Len())
-
-	repeatsI64 := int64(repeats);
-	fmt.Printf("default sort: %dns\n", durations[0].Nanoseconds() / repeatsI64)
-	fmt.Printf("comb    sort: %dns\n", durations[1].Nanoseconds() / repeatsI64)
-	fmt.Printf("insert  sort: %dns\n", durations[2].Nanoseconds() / repeatsI64)
-	fmt.Printf("pancake sort: %dns\n", durations[3].Nanoseconds() / repeatsI64)
-
-	fmt.Printf("\n===    end    ===\n")
-
-	return nil
-}
 
 func main() {
 	var (
@@ -88,7 +27,9 @@ func main() {
 	fmt.Print("Array len: ")
 	_, err = fmt.Scan(&len)
 
-	if err == nil && len <= 0 { err = fmt.Errorf("len under 1") }
+	if err == nil && len <= 0 {
+		err = fmt.Errorf("len under 1")
+	}
 
 	if err != nil {
 		fmt.Println(err)
@@ -113,10 +54,5 @@ func main() {
 		return
 	}
 
-	err = benchmark(arr)
-
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	benchmark.Benchmark(arr, *repeatsFlag)
 }
